43edc16f079417332ba82eec2819a53c4efcb3a4
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-24 00:17:24 -0500

    added some temporary styling to have the app look more like an app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import CurrentWins from './components/Wins';
+import styled from "styled-components"
 
+const AppDiv = styled.div`
+  width: 50%;
+  background-color: green;
+  margin: 0 auto;
+  border-radius: 20px;
+  font-family: 'Gajraj One', cursive;
+
+
+`
 
 
 
@@ -10,13 +20,13 @@ function App() {
   const [wins, setWins] = useState(["win1", "win2"]);
 
   return (
-    <div className="App">
-      <h1> 
+    <AppDiv className="App">
+      <h1 style={{fontSize: "20px"}}> 
        Welcone To Super-Win-Streak
       </h1>
       <button>Add a win!</button>
       <CurrentWins wins={wins}/>
-    </div>
+    </AppDiv>
   )
 }
 
